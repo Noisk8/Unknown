9e819933b490720e48a4b56bdea4d2cb839fc63e
Noisk8 <juansk8jaramillo@gmail.com>
2024-06-24 23:35:10 -0500

    de nuevo

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  title = 'todoapp';
+  sobeland = 'sobelo pai!';
+  tasks= [
+    'Instar la bazuca',
+    'Soplar en boranda',
+    'Comprar el Tufy',
+  ];
+
+  name = 'Noisk8'
+  age = '18'
+  disabled = true;
+  img='https://perfil2024.vercel.app/assets/images/noi.jpg';
+
+
+
+
+  person = {
+
+    nombre:'Pedro',
+    edad: '34',
+    avatar: 'https://perfil2024.vercel.app/assets/images/noi.jpg',
+  }
+
+  // Metodo de la clase 
+  clickHandler(){
+
+    alert('Hola')
+
+  }
+
+  changeHnadler(event: Event){
+    console.log(event)
+
+  }
+}
