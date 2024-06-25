f506da956854397a576dd039de4dcc8073eb68b0
Noisk8 <juansk8jaramillo@gmail.com>
2024-06-24 23:56:02 -0500

    los sopranos

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -6,41 +6,35 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   title = 'todoapp';
   sobeland = 'sobelo pai!';
-  tasks= [
-    'Instar la bazuca',
-    'Soplar en boranda',
-    'Comprar el Tufy',
-  ];
+  tasks = ['Instar la bazuca', 'Soplar en boranda', 'Comprar el Tufy'];
 
-  name = 'Noisk8'
-  age = '18'
+  name = 'Noisk8';
+  age = '18';
   disabled = true;
-  img='https://perfil2024.vercel.app/assets/images/noi.jpg';
-
-
-
+  img = 'https://perfil2024.vercel.app/assets/images/noi.jpg';
 
   person = {
-
-    nombre:'Pedro',
+    nombre: 'Pedro',
     edad: '34',
     avatar: 'https://perfil2024.vercel.app/assets/images/noi.jpg',
+  };
+
+  // Metodo de la clase
+  clickHandler() {
+    alert('Hola');
   }
 
-  // Metodo de la clase 
-  clickHandler(){
-
-    alert('Hola')
-
+  changeHnadler(event: Event) {
+    console.log(event);
   }
 
-  changeHnadler(event: Event){
-    console.log(event)
-
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
   }
 }
